815b1e974e8127f17eb24d96d9a63fb10f367bd6
farhana988 <farhana744255@gmail.com>
2025-02-11 17:56:29 +0600

    booking modal designed

--- src/pages/ServicePackages/ServicePackagesModal.jsx
+++ src/pages/ServicePackages/ServicePackagesModal.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+
 const ServicePackagesModal = ({ isOpen, closeModal, packageDetails }) => {
+    const {user} = useContext(AuthContext)
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -18,12 +22,12 @@ const ServicePackagesModal = ({ isOpen, closeModal, packageDetails }) => {
     closeModal();
   };
 
-  
-
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center 
-    items-center z-50">
+    <div
+      className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center 
+    items-center z-50"
+    >
       <div className="bg-white dark:bg-zinc-800 p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl lg:text-3xl font-semibold text-nav dark:text-ivory mb-4">
           Book {packageDetails.name}
@@ -31,13 +35,16 @@ const ServicePackagesModal = ({ isOpen, closeModal, packageDetails }) => {
         <form onSubmit={handleSubmit}>
           {/* Name Input */}
           <div className="mb-4">
-            <label className="block text-sm text-gray-700 dark:text-gray-300">
+            <label className="block text-sm lg:text-lg font-semibold ">
               Name
             </label>
             <input
               type="text"
               name="name"
-              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg"
+              value={user?.displayName}
+              readOnly
+              className="w-full p-2 border bg-base-300 dark:bg-zinc-500 border-gray-300
+               dark:border-gray-600 rounded-lg"
               placeholder="Enter your name"
               required
             />
@@ -45,13 +52,16 @@ const ServicePackagesModal = ({ isOpen, closeModal, packageDetails }) => {
 
           {/* Email Input */}
           <div className="mb-4">
-            <label className="block text-sm text-gray-700 dark:text-gray-300">
+            <label className="block text-sm lg:text-lg font-semibold ">
               Email
             </label>
             <input
               type="email"
               name="email"
-              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg"
+              value={user?.email}
+              readOnly
+              className="w-full p-2 border bg-base-300 dark:bg-zinc-500  border-gray-300
+               dark:border-gray-600 rounded-lg"
               placeholder="Enter your email"
               required
             />
@@ -59,13 +69,14 @@ const ServicePackagesModal = ({ isOpen, closeModal, packageDetails }) => {
 
           {/* Phone Number Input */}
           <div className="mb-4">
-            <label className="block text-sm text-gray-700 dark:text-gray-300">
+            <label className="block text-sm lg:text-lg font-semibold ">
               Phone Number
             </label>
             <input
               type="tel"
               name="phone"
-              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg"
+              className="w-full p-2 border bg-lCard dark:bg-dCard border-gray-300
+               dark:border-gray-600 rounded-lg"
               placeholder="Enter your phone number"
               required
             />
@@ -73,13 +84,14 @@ const ServicePackagesModal = ({ isOpen, closeModal, packageDetails }) => {
 
           {/* Address Input */}
           <div className="mb-4">
-            <label className="block text-sm text-gray-700 dark:text-gray-300">
+            <label className="block text-sm lg:text-lg font-semibold ">
               Address
             </label>
             <input
               type="text"
               name="address"
-              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg"
+              className="w-full p-2 border bg-lCard dark:bg-dCard border-gray-300
+               dark:border-gray-600 rounded-lg"
               placeholder="Enter your address"
               required
             />
