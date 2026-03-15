ea809f1c14e07c4b947d59f88e2ebe00718dadca
farhana988 <farhana744255@gmail.com>
2026-03-15 12:34:12 +0600

    updated support

--- src/components/FooterLinks/Support.jsx
+++ src/components/FooterLinks/Support.jsx
@@ -1,56 +1,52 @@
 import Heading from "../shared/Heading";
 
+const contactMethods = [
+  {
+    title: "Email Support",
+    content: "You can reach us at support@criticizeIt.com for any inquiries.",
+    link: "mailto:support@criticizeIt.com",
+  },
+  {
+    title: "Phone Support",
+    content: "Call us at 1-800-123-4567 for immediate assistance.",
+    link: "tel:+18001234567",
+  },
+  {
+    title: "Live Chat",
+    content:
+      "You can chat with our team directly on our website during business hours.",
+    link: null,
+  },
+];
+
 const Support = () => {
   return (
-    <div className="min-h-screen py-10 container mx-auto px-6">
+    <div className="container mx-auto">
       <Heading title={"Support"} />
-      <div className=" md:w-10/12 mx-auto space-y-6">
-        <p className="text-xl font-medium mb-4">
-          Our support team is here to help. If you have any questions or need
-          assistance, please reach out to us using one of the options below.
-        </p>
-
-        <h2 className="text-3xl font-semibold  mb-4">Contact Methods</h2>
-
+      <p className="text-sm font-medium mb-4 md:w-2/3 xl:w-1/2">
+        Our support team is here to help. If you have any questions or need
+        assistance, please reach out to us using one of the options below.
+      </p>
+      <div className="md:w-10/12 mx-auto space-y-6">
+        <h2 className="text-2xl font-semibold mb-4">Contact Methods</h2>
         <div className="space-y-6">
-          {/* Email Support */}
-          <div className=" bg-lCard dark:bg-dCard  p-6 rounded-xl shadow-md
-           hover:bg-indigo-200 transition-all border-b-4 border-primary">
-            <h3 className="text-2xl font-semibold ">Email Support</h3>
-            <p className="mt-2">
-              You can reach us at{" "}
-              <a
-                href="mailto:support@example.com"
-                className="underline"
-              >
-                support@criticizeIt.com
-              </a>{" "}
-              for any inquiries.
-            </p>
-          </div>
-
-          {/* Phone Support */}
-          <div className="  bg-lCard dark:bg-dCard    p-6 rounded-xl shadow-md
-            hover:bg-indigo-200 transition-all border-b-4 border-primary">
-            <h3 className="text-2xl font-semibold ">Phone Support</h3>
-            <p className="mt-2">
-              Call us at{" "}
-              <a href="tel:+18001234567" className="underline ">
-                1-800-123-4567
-              </a>{" "}
-              for immediate assistance.
-            </p>
-          </div>
-
-          {/* Live Chat */}
-          <div className="  bg-lCard dark:bg-dCard    p-6 rounded-xl shadow-md
-            hover:bg-indigo-200 transition-all border-b-4 border-primary">
-            <h3 className="text-2xl font-semibold ">Live Chat</h3>
-            <p className="mt-2">
-              You can chat with our team directly on our website during business
-              hours.
-            </p>
-          </div>
+          {contactMethods.map((method, index) => (
+            <div
+              key={index}
+              className="bg-lCard dark:bg-dCard p-6 rounded-xl shadow-md hover:bg-indigo-200 transition-all border-b-4 border-primary"
+            >
+              <h3 className="text-xl font-semibold">{method.title}</h3>
+              <p className="mt-2 text-xs">
+                {method.link ? (
+                  <a href={method.link} className="underline">
+                    {method.content}
+                  </a>
+                ) : (
+                  method.content
+                )}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
