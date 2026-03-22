d1defb8743ef67e687388e20bce24693c38d3899
farhana988 <farhana744255@gmail.com>
2026-03-22 21:31:38 +0600

    update

--- src/components/shared/Navbar/NavLinks.jsx
+++ src/components/shared/Navbar/NavLinks.jsx
@@ -2,14 +2,13 @@ import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 
-
 const NavLinks = () => {
   const { user } = useContext(AuthContext);
 
   const linkClass = ({ isActive }) =>
     isActive
-      ? "active text-nav dark:text-ivory text-2xl xl:text-3xl font-extrabold"
-      : "font-medium xl:text-lg text-nav dark:text-ivory hover:text-[#536493] dark:hover:text-base-300";
+      ? "active text-nav dark:text-ivory text-xl font-semibold"
+      : "font-thin text-sm text-nav dark:text-ivory hover:text-[#536493] dark:hover:text-base-300";
 
   return (
     <>
